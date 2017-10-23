ee3da174997e021f655dea640fdcf4821af1ed40
geraldmcr <33013108+geraldmcr@users.noreply.github.com>
2017-10-22 21:00:28 -0600

    Update Punto6.ino

--- Punto6.ino
+++ Punto6.ino
@@ -1,25 +1,22 @@
+//inicializa el pin del boton
+int button2 = 2;
+// inicializa el pin de los LED
+int pin3 = 3;
+// inicializa la variable donde guarda el valor del boton
+int valor = 0;
 
-  int valor = 0;
-  int button2 = 2;
-  int pin3 = 3;
-  
 void setup() {
-  Serial.begin(9600);
   pinMode(button2, INPUT);// boton
   pinMode(pin3, OUTPUT); // luz verde
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  valor = digitalRead(button2);
-  if (valor == HIGH){
-    digitalWrite(pin3, HIGH);   // turn the LED on (HIGH is the voltage level)
-    Serial.println("Encendido");
+  valor = digitalRead(button2); // guarda el valor del boton
+  if (valor == HIGH){ // si esta oprimido
+    digitalWrite(pin3, HIGH);   // Enciende el LED
   }
-  else{
-    digitalWrite(pin3, LOW); //turn the LED on LOW.
-    Serial.println("Apagado");    
-    }
-
- // delay(1000);
+  else{ // sino
+    digitalWrite(pin3, LOW); // Apaga el LED  
+  }
 }
