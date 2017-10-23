ea71bd64eb1044ea4e61e49a4c0ed6dde89b42de
geraldmcr <33013108+geraldmcr@users.noreply.github.com>
2017-10-22 20:47:13 -0600

    Update Punto9.ino

--- Punto9.ino
+++ Punto9.ino
@@ -28,32 +28,32 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  val0 = analogRead(analogPin0);
+  val0 = analogRead(analogPin0); // Lee el 1er potenciometro
   brillo3 = val0/4;
-  porcentaje3 = (brillo3/255)*100;
+  porcentaje3 = (brillo3/255)*100; // convierte el valor leido a %
 
-  val1 = analogRead(analogPin1);
+  val1 = analogRead(analogPin1); // Lee el 2do potenciometro
   brillo5 = val1/4;
-  porcentaje5 = (brillo5/255)*100;
+  porcentaje5 = (brillo5/255)*100; // convierte el valor leido a %
 
-  val2 = analogRead(analogPin2);
+  val2 = analogRead(analogPin2); // Lee el 3er potenciometro
   brillo6 = val2/4;
-  porcentaje6 = (brillo6/255)*100;
+  porcentaje6 = (brillo6/255)*100; // convierte el valor leido a %
    
-  analogWrite(analogPin0, brillo3);
+  analogWrite(analogPin0, brillo3); // indica intensidad al led
   analogWrite(pin3,brillo3);
   String s1 = "Porcentaje de Azul: ";
-  Serial.println(s1 + porcentaje3 + "%"); 
+  Serial.println(s1 + porcentaje3 + "%"); // imprime el % de intensidad del LED
   
-  analogWrite(analogPin1, brillo5);   
+  analogWrite(analogPin1, brillo5); // indica intensidad al led
   analogWrite(pin5,brillo5);
   String s2 = "Porcentaje de Verde: ";
-  Serial.println(s2 + porcentaje5 + "%");
+  Serial.println(s2 + porcentaje5 + "%"); // imprime el % de intensidad del LED
 
-  analogWrite(analogPin2, brillo6);
+  analogWrite(analogPin2, brillo6); // indica intensidad al led
   analogWrite(pin6,brillo6);
   String s3 = "Porcentaje de Rojo: "; 
-  Serial.println(s3 + porcentaje6 + "%");
+  Serial.println(s3 + porcentaje6 + "%"); // imprime el % de intensidad del LED
 
   delay(1000);
 }
