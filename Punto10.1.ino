000240bfafb5baa681530954b2fa83cbbedd3f8e
geraldmcr <33013108+geraldmcr@users.noreply.github.com>
2017-10-22 21:22:39 -0600

    Update Punto10.1.ino

--- Punto10.1.ino
+++ Punto10.1.ino
@@ -1,17 +1,12 @@
-/*
- 
-*/
+// se inicializan los pines y variables requeridos
+int button2 = 2;
+int pin3 = 3;
+int pin5 = 5;
+int pin6 = 6; 
+int counter = 0;
 
-  int button2 = 2;
-  int pin3 = 3;
-  int pin5 = 5;
-  int pin6 = 6; 
-  int counter = 0;
-  
-  
 // the setup function runs once when you press reset or power the board
 void setup() {
-  Serial.begin(9600);
   pinMode(button2, INPUT); // botón
   pinMode(pin3, OUTPUT); // luz verde
   pinMode(pin5, OUTPUT); // luz amarilla
@@ -65,91 +60,90 @@ int salidaLuces(int pin01, int pin02, int pin03, int a, int b, int c){
 
 // the loop function runs over and over again forever
 void loop() {
-    
-    WAIT:
-      while (digitalRead(button2) == LOW){
-        salidaLuces(pin3,pin5,pin6,0,0,0); //NEGRO
-      }
+  WAIT:
+    while (digitalRead(button2) == LOW){
+      salidaLuces(pin3,pin5,pin6,0,0,0); //NEGRO
+    }
 
-    goto N;
-    
-    N: 
-     salidaLuces(pin3,pin5,pin6,0,0,0); //NEGRO
-     delay(500);
-     if (digitalRead(button2) == HIGH) {
-       goto A;
-     }
-       else{
-         goto WAIT;
-        }
+  goto N;
 
-    A: 
-      salidaLuces(pin3,pin5,pin6,0,0,1); //AZUL
-      delay(500);
-      if (digitalRead(button2) == HIGH){
-        goto V;
-      }
-      else{
-        goto WAIT;
-      }
-    
-    V: 
-      salidaLuces(pin3,pin5,pin6,0,1,0); //VERDE
-      delay(500);
-      if (digitalRead(button2) == HIGH){
-        goto AV;
-      }
-      else{
-        goto WAIT;
-      }
-    
-    AV: 
-      salidaLuces(pin3,pin5,pin6,0,1,1); //AZUL + VERDE
-      delay(500);
-      if (digitalRead(button2) == HIGH){
-        goto R;
-      }
-      else{
-        goto WAIT;
-      }
+  N: 
+    salidaLuces(pin3,pin5,pin6,0,0,0); //NEGRO
+    delay(500);
+    if (digitalRead(button2) == HIGH) {
+      goto A;
+    }
+    else{
+      goto WAIT;
+    }
 
-    R: 
-      salidaLuces(pin3,pin5,pin6,1,0,0); //ROJO
-      delay(500);
-      if (digitalRead(button2) == HIGH){
-        goto RA;
-      }
-      else{
-        goto WAIT;
-      }
-    
-    RA: 
-      salidaLuces(pin3,pin5,pin6,1,0,1); //ROJO + AZUL
-      delay(500);
-      if (digitalRead(button2) == HIGH){
-        goto RV;
-      }
-      else{
-        goto WAIT;
-      }
+  A: 
+    salidaLuces(pin3,pin5,pin6,0,0,1); //AZUL
+    delay(500);
+    if (digitalRead(button2) == HIGH){
+      goto V;
+    }
+    else{
+      goto WAIT;
+    }
 
-    RV: 
-      salidaLuces(pin3,pin5,pin6,1,1,0); //ROJO + VERDE
-      delay(500);
-      if (digitalRead(button2) == HIGH){
-        goto B;
-      }
-      else{
-        goto WAIT;
-      }
+  V: 
+    salidaLuces(pin3,pin5,pin6,0,1,0); //VERDE
+    delay(500);
+    if (digitalRead(button2) == HIGH){
+      goto AV;
+    }
+    else{
+      goto WAIT;
+    }
 
-    B: 
-      salidaLuces(pin3,pin5,pin6,1,1,1); //BLANCO
-      delay(500);
-      if (digitalRead(button2) == HIGH){
-        goto N;
-      }
-      else{
-        goto WAIT;
-      } 
+  AV: 
+    salidaLuces(pin3,pin5,pin6,0,1,1); //AZUL + VERDE
+    delay(500);
+    if (digitalRead(button2) == HIGH){
+      goto R;
+    }
+    else{
+      goto WAIT;
+    }
+
+  R: 
+    salidaLuces(pin3,pin5,pin6,1,0,0); //ROJO
+    delay(500);
+    if (digitalRead(button2) == HIGH){
+      goto RA;
+    }
+    else{
+      goto WAIT;
+    }
+
+  RA: 
+    salidaLuces(pin3,pin5,pin6,1,0,1); //ROJO + AZUL
+    delay(500);
+    if (digitalRead(button2) == HIGH){
+      goto RV;
+    }
+    else{
+      goto WAIT;
+    }
+
+  RV: 
+    salidaLuces(pin3,pin5,pin6,1,1,0); //ROJO + VERDE
+    delay(500);
+    if (digitalRead(button2) == HIGH){
+      goto B;
+    }
+    else{
+      goto WAIT;
+    }
+
+  B: 
+    salidaLuces(pin3,pin5,pin6,1,1,1); //BLANCO
+    delay(500);
+    if (digitalRead(button2) == HIGH){
+      goto N;
+    }
+    else{
+      goto WAIT;
+    } 
 }
