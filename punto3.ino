e996ba8e4d635edfff8fac02a109f8b98496c670
geraldmcr <33013108+geraldmcr@users.noreply.github.com>
2017-10-22 20:53:43 -0600

    Update punto3.ino

--- punto3.ino
+++ punto3.ino
@@ -1,13 +1,12 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, HIGH);   // Enciende el LED
+  delay(5000);                       // lo enciende por 5 segundos
+  digitalWrite(LED_BUILTIN, LOW);    // Apaga el LED
   delay(1000);                       // wait for a second
 }
