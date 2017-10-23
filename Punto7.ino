73a0e8183c5579271e4332a44b5f00d66c7da53f
geraldmcr <33013108+geraldmcr@users.noreply.github.com>
2017-10-22 21:03:32 -0600

    Update Punto7.ino

--- Punto7.ino
+++ Punto7.ino
@@ -1,43 +1,36 @@
-/*
-  Blink
-*/
+// inicializa los pines de los LED's y el boton
+int pin3 = 3;
+int pin5 = 5;
+int pin6 = 6;
+int button2 = 2;
+// inicializa la variable donde guarda el valor del boton
+int valor = 0;  
 
-  int pin3 = 3;
-  int pin5 = 5;
-  int pin6 = 6;
-  int valor = 0;
-  int button2 = 2;
-  
 // the setup function runs once when you press reset or power the board
 void setup() {
-  Serial.begin(9600);
   pinMode(button2, INPUT);// boton
   pinMode(pin3, OUTPUT); // luz verde
   pinMode(pin5, OUTPUT); // luz amarilla
   pinMode(pin6, OUTPUT); // luz roja
-  
 }
 
 // the loop function runs over and over again forever
 void loop() {
-   valor = digitalRead(button2);
+  valor = digitalRead(button2);
   if (valor == HIGH){
-  // ENCIENDE EL LED DEL PIN 3
-  digitalWrite(pin3, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(pin3, LOW);  
-  
-  // ENCIENDE EL LED DEL PIN 5
-  digitalWrite(pin5, HIGH);
-  delay(1000);                       // wait for a second
-  digitalWrite(pin5, LOW);  
-  
-  // ENCIENDE EL LED DEL PIN 6
-  digitalWrite(pin6, HIGH);
-  delay(1000);                       // wait for a second
-  digitalWrite(pin6, LOW);  
-    }
-  else{
-    Serial.println("Apagado");    
-    }
+    // ENCIENDE EL LED DEL PIN 3
+    digitalWrite(pin3, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(pin3, LOW);  
+
+    // ENCIENDE EL LED DEL PIN 5
+    digitalWrite(pin5, HIGH);
+    delay(1000);                       // wait for a second
+    digitalWrite(pin5, LOW);  
+
+    // ENCIENDE EL LED DEL PIN 6
+    digitalWrite(pin6, HIGH);
+    delay(1000);                       // wait for a second
+    digitalWrite(pin6, LOW);  
+  }
 }
